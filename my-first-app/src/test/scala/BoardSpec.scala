80dae1bc2de67c9cfb6e5e068df172ce5b1d7bdd
mshk1n <zipperik16@gmail.com>
2026-05-25 13:01:32 +0200

    BoardSpec fix for new types

--- my-first-app/src/test/scala/BoardSpec.scala
+++ my-first-app/src/test/scala/BoardSpec.scala
@@ -17,24 +17,24 @@ class BoardSpec extends AnyWordSpec with Matchers {
       rendered should include("---")
     }
 
-    "return false for out-of-bounds columns" in {
+    "return None for out-of-bounds columns" in {
       val board = new Board()
-      board.dropChip(-1, "X") should be(false)
-      board.dropChip(7, "X") should be(false)
+      board.dropChip(-1, "X") should be(None)
+      board.dropChip(7, "X") should be(None)
     }
 
     "correctly find the lowest empty row" in {
       val board = new Board()
-      board.dropChip(0, "X") should be(true)
+      board.dropChip(0, "X") should be(Some((5, 0)))
       board.getCell(5, 0) should be("X")
-      board.dropChip(0, "O") should be(true)
+      board.dropChip(0, "O") should be(Some((4, 0)))
       board.getCell(4, 0) should be("O")
     }
 
     "return false if column is completely full" in {
       val board = new Board()
       for (_ <- 0 until 6) board.dropChip(0, "X")
-      board.dropChip(0, "Y") should be(false)
+      board.dropChip(0, "Y") should be(None)
     }
   }
 }
